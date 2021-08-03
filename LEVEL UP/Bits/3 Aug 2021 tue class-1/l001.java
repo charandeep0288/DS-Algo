2a9f70677988afe8a0b44b44c3b9be70e66c5832
Charandeep <ckaunsel123@gmail.com>
2021-08-03 19:21:55 +0530

    bits class 1

---
+++ LEVEL UP/Bits/3 Aug 2021 tue class-1/l001.java
@@ -0,0 +1,27 @@
+public class l001 {
+    
+    // 1 -> 0, 0 -> 0 / false -> true, true -> true
+    public static void onToOff(int n, int k){
+        
+    }
+
+    // 1 -> 0, 0 -> 0 / true -> false, false -> false
+    public static void offToOn(int n, int k){
+
+    }
+
+    // jtna 1 exist karta hai unka count return karna hai
+    public static int countSetBits( int n ){
+
+        return 0;
+    }
+    public static void main(String[] args){
+
+        int n = 7;
+        // n = !n; // not allowed, compilatoin error
+        n = ~n; // allowed
+        
+        System.out.println();
+
+    }
+}
